a2747f609e17041ad816a469d40ae3da1b0f4b2c
Javi Granados <jjavigrana2@gmail.com>
2025-04-08 14:13:34 +0200

    header con movimiento de coche

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-const imagenesInicio = new Array("img/inicio1.png", "img/inicio2.png", "img/inicio3.png", "img/inicio4.png");
+const imagenesInicio = new Array("img/inicio1.png", "img/inicio2.png", "img/inicio3.jpg", "img/inicio4.png");
 const fotoInicio = document.getElementById("foto");
 let numFoto = 0;
 const intervalTime = 3000; // Cambia cada 3 segundos
 
 window.addEventListener('load', function(){
     fotoInicio.src = imagenesInicio[numFoto];
-    //console.log(numFoto);
+    console.log(numFoto);
 });
 
 function nextImage() {
@@ -16,7 +16,7 @@ function nextImage() {
     fotoInicio.src = imagenesInicio[numFoto];
 }
 
-setInterval(nextImage, intervalTime);
+setTimeout(nextImage, intervalTime);
 
 
 //MOVIMIENTO DE LA CABECERA
@@ -24,13 +24,21 @@ const header = document.querySelector("header");
 const expandBtn = document.querySelector(".expand-btn");
 let headerShrunk = false;
 
+function moverCoche(){
+  let coche = document.getElementById("headerCar");
+  coche.style.paddingLeft = "46.5%"
+}
+
 // Se achica el header solo una vez al hacer scroll
 window.addEventListener("scroll", () => {
   if (!headerShrunk && window.scrollY > 50) {
     header.classList.add("shrink");
     headerShrunk = true;
+    setInterval(moverCoche, 500);
   }
 });
+
+
 
 
 //MOVIMIENTO FOOTER
