02b3ec0213f2f8161234488ed2639abf729c9a05
Javi Granados <jjavigrana2@gmail.com>
2025-04-09 00:32:48 +0200

    cambios contenido

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,3 @@
-const imagenesInicio = new Array("1", "2", "3", "4");
-const fotoInicio = document.getElementById("foto");
-let numFoto = 1;
-const intervalTime = 3000; // Cambia cada 3 segundos
-
-window.addEventListener('load', function(){
-    fotoInicio.src = `/UdgRacingDivision/img/inicio${numFoto}.png`;
-});
-
-function nextImage() {
-    numFoto++;
-    if (numFoto == 5) {
-        numFoto = 1;
-    }
-    if(numFoto==1)
-      fotoInicio.src = `/UdgRacingDivision/img/inicio${numFoto}.png`;
-    else
-    fotoInicio.src = `/UdgRacingDivision/img/inicio${numFoto}.JPG`;
-    console.log(fotoInicio.src);
-    console.log(`Numero de la foto: ${numFoto}`);
-}
-
-setInterval(nextImage, intervalTime);
-
 
 //MOVIMIENTO DE LA CABECERA
 const header = document.querySelector("header");
@@ -53,14 +29,14 @@ window.addEventListener("scroll", () => {
   }
 });
 
-//manu hamburguesa
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-const span = document.querySelectorAll(".hamburger span");
+// //menu hamburguesa
+// const hamburger = document.getElementById('hamburger');
+// const navLinks = document.getElementById('nav-links');
+// const span = document.querySelectorAll(".hamburger span");
 
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-  span.forEach(span => {
-    span.style.backgroundColor = navLinks.classList.contains('show') ? 'white' : 'darkblue';
-  });
-});
+// hamburger.addEventListener('click', () => {
+//   navLinks.classList.toggle('show');
+//   span.forEach(span => {
+//     span.style.backgroundColor = navLinks.classList.contains('show') ? 'white' : 'darkblue';
+//   });
+// });
