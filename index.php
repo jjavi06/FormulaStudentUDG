e1accc2f23798685125e4339820978894e1007c4
Javi Granados <jjavigrana2@gmail.com>
2025-05-31 20:10:14 +0200

    Carrules noticias del inicio hecho dinámico, se cogen las noticias de la bdd

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KRM78JZB');
+    </script>
+    <!-- End Google Tag Manager -->
+    <!-- FAVICON (Icono página) -->
+    <link rel="icon" type="image/png" href="/racingdivision/img/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/racingdivision/img/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="/racingdivision/img/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/racingdivision/img/favicon/apple-touch-icon.png" />
+    <link rel="manifest" href="/racingdivision/img/favicon/site.webmanifest" />
+    <!-- Fin favicon -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UdG Racing Division</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/racingdivision/css/style.css">
+    <link rel="stylesheet" href="/racingdivision/css/inicio.css">
+    <!--Canonical version-->
+    <link rel="canonical" href="https://epsapps.udg.edu/racingdivision/">
+</head>
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KRM78JZB"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+<!--CABECERA DE LA PÁGINA-->
+    <header>
+        <img src="/racingdivision/img/coche-header.png" class="logo, carLogo" id="headerCar">
+        <a href="/racingdivision/index.html" class="logo" id="headerLogo"><img src="/racingdivision/img/logo.png"></a>
+        <nav class="navbar">
+            <div class="hamburger" id="hamburger">
+                <img id="hamburgerImg" src="/racingdivision/img/hamburger-blue.png">
+            </div>
+        </nav>
+    </header>
+    <!-- Links del menú hamburguesa -->
+     <div class="hamburger-links">
+        <ul class="nav-links" id="nav-links">
+            <li><select id="language-select-mobile" class="lang-sel-mobile">
+                <option value="es">ES</option>
+                <option value="ca">CA</option>
+                <option value="en">EN</option>
+            </select></li>
+            <li><a href="/racingdivision/index.html" data-i18n="inicio.home">Home</a></li>
+            <li><a href="/racingdivision/sections/aboutUs.html" data-i18n="inicio.aboutUs">About Us</a></li>
+            <li><a href="/racingdivision/sections/sponsors&collaborators.php" data-i18n="inicio.sponsors">Sponsors &<br>Collaborators</a></li>
+            <li><a href="/racingdivision/sections/news.php" data-i18n="inicio.news">News</a></li>
+            <li><a href="/racingdivision/sections/contacto.php" data-i18n="inicio.contact">Get In Touch</a></li>
+            <img src="/racingdivision/img/logo-blanco.png" alt="">
+        </ul>
+     </div>
+<!--MENÚ DE NAVEGACIÓN-->
+    <nav class="menu">
+        <ul class="nav-links">
+            <li><a href="/racingdivision/index.html" data-i18n="inicio.home">Home</a></li>
+            <li><a href="/racingdivision/sections/aboutUs.html" data-i18n="inicio.aboutUs">About Us</a></li>
+            <li><a href="/racingdivision/sections/sponsors&collaborators.php" data-i18n="inicio.sponsors">Sponsors [&] Collaborators</a></li>
+            <li><a href="/racingdivision/sections/news.php" data-i18n="inicio.news">News</a></li>
+            <li><a href="/racingdivision/sections/contacto.php" data-i18n="inicio.contact">Get In Touch</a></li>
+            <select id="language-select-pc" class="lang-sel-pc">
+                <option value="es">ES</option>
+                <option value="ca">CA</option>
+                <option value="en">EN</option>
+            </select>
+        </ul>
+    </nav>
+<!--CONTENIDO CENTRAL-->
+    <section class="white-section">
+        <article class="parrafo-inicio">
+            <div>
+                <h2 data-i18n="inicio.title1">The 2026 challenge begins!</h2>
+                <p data-i18n="inicio.p1">Behind every bolt, every tube and every curve of this single-seater lie months of design, dedication and passion. This is the result of the collective effort of UdG Racing Division, a multidisciplinary team of students from the University of Girona already gearing up for the 2026 Formula Student Spain season.</p>
+                <p data-i18n="inicio.p2">This machine is more than speed: it’s learning, innovation, and the reflection of a university community united by a shared goal.</p>
+            </div>
+            <img src="/racingdivision/img/inicio1.jpg" class="img-inicio">
+        </article>
+        <article class="parrafo-inicio">
+            <img src="/racingdivision/img/inicio2.jpg" class="img-inicio" id="img-medio-arriba">
+            <div>
+                <h2 data-i18n="inicio.title2">More than a team, a technical family</h2>
+                <p data-i18n="inicio.p3">This is the true driving force behind UdG Racing Division: a group of students, faculty, and collaborators brought together by a shared passion and a common goal.</p>
+                <p data-i18n="inicio.p4">Behind every component of our race car lies countless hours of effort, debate, innovation, and valuable learning. Formula Student is more than a competition, it’s a school of engineering and life.</p>
+                <p data-i18n="inicio.p5">Thank you to everyone who’s part of this journey. The road to 2026 is only possible with you.</p>
+            </div>
+            <img src="/racingdivision/img/inicio2.jpg" class="img-inicio" id="img-medio-abajo">
+        </article>
+        <article class="parrafo-inicio">
+            <div>
+                <h2 data-i18n="inicio.title3">Every detail matters</h2>
+                <p data-i18n="inicio.p6">Precision, analysis, and constant review are key to achieving excellence. At UdG Racing Division, every part is meticulously checked to ensure our race car meets the highest standards of safety, performance, and innovation.</p>
+                <p data-i18n="inicio.p7">It’s not just about building a car: it’s about understanding it, testing, failing, and improving, always as a team. Because the true power behind this project isn’t the engine, it’s the minds that bring it to life.</p>
+            </div>
+            <img src="/racingdivision/img/inicio3.jpg" class="img-inicio">
+        </article>
+    </section>
+    <section class="contenido-inicio, blue-section">
+        <hr>
+        <!--CARRUSEL NOTICIAS-->
+        <article class="blue-article">
+            <h2 data-i18n="inicio.title4">News</h2>
+            <div id="noticias">
+                <button class="carousel-btn prev-btn" aria-label="Previous news">&lt;</button>
+                <div class="slides-container">
+                    <?php
+                        require 'php/dbconnection.php';
+                        require 'php/claseNoticia.php';
+                        // Coge el valor de lang pasado por noticias.js, valor por defecto -> es
+                        $lang = $_GET['lang'] ?? 'es';
+
+                        $SQL = 'SELECT id, shortDesc, largeDesc, lang, foto
+                            FROM noticias where lang = :lang
+                            ORDER BY relevant DESC, id DESC 
+                            LIMIT 6';
+                        $rst = $conn -> prepare($SQL);
+                        $rst -> bindParam(":lang", $lang);
+                        $rst -> execute();
+                        $rst -> setFetchMode(PDO::FETCH_CLASS, 'Noticia');
+                        $carruselNews = $rst -> fetchAll();
+                        
+                        foreach($carruselNews as $cNews){
+                            echo '                    
+                            <div class="slide">
+                            <a href="/racingdivision/noticias/noticia.php?id=' . $cNews->getId() . '&lang=' . $cNews->getLang() . '">
+                                <img src="/racingdivision/noticias/img/' . $cNews -> getFoto() . '">
+                            </a>
+                            <p>' . $cNews -> getShortDesc() . '</p>
+                            </div>';
+                        }
+                    ?>
+                </div>
+                <button class="carousel-btn next-btn" aria-label="Next news">&gt;</button>
+            </div>
+        </article>
+        <!--SPONSORS-->
+        <hr>
+        <article id="sponsors">
+            <h2 data-i18n="inicio.title5">Sponsors and Collaborators</h2>
+            <div>
+                <a href="https://www.eic.cat/" target="_blank"><img src="/racingdivision/img/white-eic-logo.png"></a>
+                <a href="https://patronateps.udg.edu/" target="_blank"><img src="/racingdivision/img/white-patronat-logo.png"></a>
+                <a href="https://www.udg.edu/ca/" target="_blank"><img src="/racingdivision/img/white-udg-logo.png"></a>
+            </div>
+        </article>
+        <hr>
+    </section>
+<!--PIE DE PÁGINA-->
+    <footer class="pie-pag">
+        <div class="contenido-footer" id="footer-general">
+            <ul>
+                <li><img src="/racingdivision/img/blue-udg-logo.png" class="footer-logo"></li>
+            </ul>
+            <ul>
+                <li>C/ Maria Aurèlia Capmany i</li>
+                <li>Farnés Nº 61</li>
+                <li>17003 Girona</li>
+            </ul>
+            <ul>
+                <li><img src="/racingdivision/img/insta-logo.png" class="icono-footer"><a href="https://www.instagram.com/udgracingdivision/" target="_blank"> Instagram</a></li>
+                <li><img src="/racingdivision/img/tiktok-logo.png" class="icono-footer"><a href="https://www.tiktok.com/@udgracingdivision" target="_blank"> TikTok</a></li>
+                <li><img src="/racingdivision/img/linkedin-logo.png" class="icono-footer"><a href="https://www.linkedin.com/company/udg-racing-division-1/" target="_blank">Linkedin</a></li>
+            </ul>
+            <ul>
+                <li>info@udgracingdivision.cat</li>
+                <li><a href="/racingdivision/sections/contacto.php" data-i18n="inicio.contactFooter">Contact</a></li>
+                <li><a href="/racingdivision/sections/contacto.php" data-i18n="inicio.sponsorFooter">Become a Sponsor</a></li>
+            </ul>
+        </div>
+        <!-- footer movil -->
+        <div id="footer-movil">
+            <ul class="iconos-movil">
+                <li><a href="https://www.instagram.com/udgracingdivision/" target="_blank"><img src="/racingdivision/img/insta-logo.png" class="icono-footer"></a></li>
+                <li><a href="https://www.tiktok.com/@udgracingdivision" target="_blank"><img src="/racingdivision/img/tiktok-logo.png" class="icono-footer"></a></li>
+                <li><a href="https://www.linkedin.com/company/udg-racing-division-1/" target="_blank"><img src="/racingdivision/img/linkedin-logo.png" class="icono-footer"></a></li>
+                <li><a href="/racingdivision/sections/contacto.php"><img src="/racingdivision/img/phone-logo.png" class="icono-footer"></a></li>
+            </ul>
+            <div class="contenido-fmovil">
+                <a href="https://www.udg.edu/ca/" target="_blank"><img src="/racingdivision/img/udg-small-logo.png" class="small-logo"></a>
+                <p>C/ Maria Aurèlia Capmany i Farnés Nº 61, 17003 Girona</p>
+            </div>
+        </div>
+    </footer>
+    <div id="createdBy">Created by Javier Granados - Contact: jmelekhov@gmail.com</div>
+    <script src="/racingdivision/scripts/noticias.js"></script>
+    <script src="/racingdivision/scripts/script.js"></script>
+    <script src="/racingdivision/scripts/inicio.js"></script>
+</body>
+</html>
